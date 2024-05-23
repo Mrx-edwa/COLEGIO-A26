c53c5fdc874b0d462c0cdc6653a855ef63211fd1
Mrx-edwa <112603708+Mrx-edwa@users.noreply.github.com>
2024-05-23 11:50:54 -0500

    edwarteamo

--- mail/contact.php
+++ mail/contact.php
@@ -1,6 +1,6 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
+if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(400); // Bad Request
   exit();
 }
 
@@ -9,12 +9,16 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "edwarxr3@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$to = "edwarxr3@gmail.com"; // Cambia este correo al tuyo //
+$subject = "$m_subject: $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage:\n$message";
+$header = "From: noreply@yourdomain.com\n"; // Es mejor usar un correo fijo para evitar problemas de SPF
+$header .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if (!mail($to, $subject, $body, $header)) {
+  http_response_code(500); // Internal Server Error
+  exit();
+} else {
+  http_response_code(200); // OK
+}
 ?>
